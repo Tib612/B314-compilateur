d8e7aa625ca623352d63ccca1e384a8130798cd6
Dao Thuy Ngan <thuyngandao@gmail.com>
2018-03-15 23:26:41 +0100

    Restore the tests

--- src/test/java/be/unamur/info/b314/compiler/main/B314commentsSyntaxTest.java
+++ src/test/java/be/unamur/info/b314/compiler/main/B314commentsSyntaxTest.java
@@ -26,34 +26,34 @@ public class B314commentsSyntaxTest {
     };
 
     
-    // // Serie comments OK
+    // Serie comments OK
 
-    // @Test
-    // public void testcomments_comments_everywhere_ok() throws Exception{
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ok/comments_everywhere.b314", testFolder.newFile(), true, "comments: comments_everywhere");
-    // }
+    @Test
+    public void testcomments_comments_everywhere_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/comments/ok/comments_everywhere.b314", testFolder.newFile(), true, "comments: comments_everywhere");
+    }
 
-    // @Test
-    // public void testcomments_empty_ok() throws Exception{
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ok/empty.b314", testFolder.newFile(), true, "comments: empty");
-    // }
+    @Test
+    public void testcomments_empty_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/comments/ok/empty.b314", testFolder.newFile(), true, "comments: empty");
+    }
 
-    // @Test
-    // public void testcomments_empty_multiple_lines_and_tab_ok() throws Exception{
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ok/empty_multiple_lines_and_tab.b314", testFolder.newFile(), true, "comments: empty_multiple_lines_and_tab");
-    // }
+    @Test
+    public void testcomments_empty_multiple_lines_and_tab_ok() throws Exception{
+        CompilerTestHelper.launchCompilation("/syntax/comments/ok/empty_multiple_lines_and_tab.b314", testFolder.newFile(), true, "comments: empty_multiple_lines_and_tab");
+    }
 
-    // // par tndao
-    // @Test
-    // public void test_example_description_monde_ok() throws Exception {
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ok/description_monde.b314", testFolder.newFile(), true, "Description du monde");
-    // }
+    // par tndao
+    @Test
+    public void test_example_description_monde_ok() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/comments/ok/description_monde.b314", testFolder.newFile(), true, "Description du monde");
+    }
 
-    // // par tndao
-    // @Test
-    // public void test_parentheses_ok() throws Exception {
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ok/parentheses_intExp.b314", testFolder.newFile(), true, "Strategy");
-    // }
+    // par tndao
+    @Test
+    public void test_parentheses_ok() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/comments/ok/parentheses_intExp.b314", testFolder.newFile(), true, "Strategy");
+    }
 
     // par Thibaut
     @Test
@@ -61,34 +61,34 @@ public class B314commentsSyntaxTest {
         CompilerTestHelper.launchCompilation("/syntax/comments/ok/symbolTable.b314", testFolder.newFile(), true, "symbolTable");
     }
 
-    // // Serie comments KO
+    // Serie comments KO
 
-    // @Test
-    // public void testcomments_at_least_one_instruction_in_default_ko() throws Exception {
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ko/at_least_one_instruction_in_default.b314", testFolder.newFile(), false, "comments: at_least_one_instruction_in_default");
-    // }
+    @Test
+    public void testcomments_at_least_one_instruction_in_default_ko() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/comments/ko/at_least_one_instruction_in_default.b314", testFolder.newFile(), false, "comments: at_least_one_instruction_in_default");
+    }
 
-    // @Test
-    // public void testcomments_missing_declare_bloc_ko() throws Exception {
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ko/missing_declare_bloc.b314", testFolder.newFile(), false, "comments: missing_declare_bloc");
-    // }
+    @Test
+    public void testcomments_missing_declare_bloc_ko() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/comments/ko/missing_declare_bloc.b314", testFolder.newFile(), false, "comments: missing_declare_bloc");
+    }
 
-    // @Test
-    // public void testcomments_missing_default_bloc_ko() throws Exception {
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ko/missing_default_bloc.b314", testFolder.newFile(), false, "comments: missing_default_bloc");
-    // }
+    @Test
+    public void testcomments_missing_default_bloc_ko() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/comments/ko/missing_default_bloc.b314", testFolder.newFile(), false, "comments: missing_default_bloc");
+    }
 
-    // //ajouté par Thibaut
-    // @Test
-    // public void testcomments_wrong_array_size_ko() throws Exception {
-    //     CompilerTestHelper.launchCompilation("/syntax/comments/ko/wrong_array_size_ko.b314", testFolder.newFile(), false, "comments: wrong_array_size");
-    // }
+    //ajouté par Thibaut
+    @Test
+    public void testcomments_wrong_array_size_ko() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/comments/ko/wrong_array_size_ko.b314", testFolder.newFile(), false, "comments: wrong_array_size");
+    }
 
-    // // par tndao
-    // @Test
-    // public void testLHSExpression_VariableUndefined_ko() throws Exception {
-    //     CompilerTestHelper.launchCompilation("/syntax/exprG/variable_undefined_ko.b314", testFolder.newFile(), false, "Left-hand-side expression: variable undefined");
-    // }
+    // par tndao
+    @Test
+    public void testLHSExpression_VariableUndefined_ko() throws Exception {
+        CompilerTestHelper.launchCompilation("/syntax/exprG/variable_undefined_ko.b314", testFolder.newFile(), false, "Left-hand-side expression: variable undefined");
+    }
 
     // par tndao
     @Test
