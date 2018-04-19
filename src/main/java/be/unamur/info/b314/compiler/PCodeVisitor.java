237a8a7856a7f7a51acb03f25e15cda6e16c29a7
Dao Thuy Ngan <thuyngandao@gmail.com>
2018-04-19 13:10:53 +0200

    Try using input and output. I don't know if it work @@

--- src/main/java/be/unamur/info/b314/compiler/PCodeVisitor.java
+++ src/main/java/be/unamur/info/b314/compiler/PCodeVisitor.java
@@ -14,12 +14,53 @@ public class PCodeVisitor extends B314BaseVisitor<Object> {
     private final Map<String, Integer> symTable;
 
     private final PCodePrinter printer;
+    private final int nEnvVars = 99;
 
     public PCodeVisitor(Map<String, Integer> symTable, PCodePrinter printer) {
         this.symTable = symTable;
         this.printer = printer;
     }
 
+    @Override 
+    public Object visitProgramme(B314Parser.ProgrammeContext ctx) { 
+        initEnvironmentVariables();
+        printer.printComments("Start program");
+        super.visitProgramme(ctx); // visit childrens and print inside instructions
+        
+        // Trick, placeholder.
+        // TODO: Move this code to appropriate position 
+        printer.printSetStackPointer(nEnvVars + 1);
+        printer.printLoadAdress(PCodeTypes.Int, 0, nEnvVars);
+        printer.printLoadConstant(PCodeTypes.Int, 0); // next do nothing
+        printer.printStore(PCodeTypes.Int);
+        printer.printPrin();
+
+        printer.printComments("End program");
+        printer.printStop();
+        return visitChildren(ctx); 
+    }
+
+    /**
+     * Define and initialize the values of the environment variables 
+     *      using the user provided values 
+     * ref. specification: 
+     *      - section 10.1
+     *      - section 5.1.2 (La signification des differentes variables)
+     *      - section 5.2.2 (ennemi is...)
+     *      - section 5.3.2 (nearby...)
+     */
+    private void initEnvironmentVariables() {
+        printer.printComments("Initialize program");
+        printer.printSetStackPointer(nEnvVars);
+
+        // TODO: check this 
+        for (int i = 0; i < nEnvVars; i++) {
+            printer.printLoadAdress(PCodeTypes.Int, 0, i);
+            printer.printRead();
+            printer.printStore(PCodeTypes.Int);
+        }
+    }
+    
     // @Override
     // public Object visitDemo(DEMOParser.DemoContext ctx) {
     //     printer.printSetStackPointer(symTable.size()); // Reserve space for variables
