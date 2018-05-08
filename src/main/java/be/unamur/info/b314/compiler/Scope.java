5f8a1d12edfff90af1f61dfdf5f40d36b38e4285
Tib612 <thibaut.capuano@student.unamur.be>
2018-05-08 11:49:35 +0200

    fix error due to refactoring

--- src/main/java/be/unamur/info/b314/compiler/Scope.java
+++ src/main/java/be/unamur/info/b314/compiler/Scope.java
@@ -57,6 +57,7 @@ public class Scope {
 
     public List<IdInfo> getIdInfoOrderedByAddressPCode(){
         List<IdInfo> mapValues = new ArrayList<>(scope.values());
+        mapValues.removeIf(obj -> obj.getIdType().equals("fct"));
         Collections.sort(mapValues, Comparator.comparing(IdInfo::getAddressPCode));
         return mapValues;
     }
